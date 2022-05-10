5546a3ede90500ce892823d64a75ee93672547ea
Juan David Lizarralde Niño <nino.davidgo@gmail.com>
2022-05-09 19:18:04 -0500

    Create class 02

---
+++ class02/index.js
@@ -0,0 +1,51 @@
+// Condicionales
+
+// if
+/*
+if (true) { // => si (condicion)
+    console.log("Hello"); // => bloque de código a ejectuar - las instrucciones
+}; 
+*/
+
+// else if
+/*
+const datoUsuario = prompt("Ingrese una cadena");
+if (datoUsuario === "HELLO") {
+    alert("Excelent");
+} 
+else if (datoUsuario === "BYE") {
+    alert("Bad!");
+};
+*/
+
+// else => cualquier otro caso. El default
+/*
+else (datoUsuario === "Jeje") {
+    return "Heloo";
+};
+*/
+
+var condicion = 1 == "1";
+condicion;
+
+/* Tipo de dato */
+var condicion2 = "1" === 1;
+condicion2;
+
+var condicion3 = "1" !== 2;
+condicion3;
+
+var cuatro = 1 < 2;
+cuatro;
+
+var cinco = 1 > 2;
+cinco;
+
+var seis = 1 <= 2;
+seis;
+
+var siete = 1 >= 2;
+siete;
+
+var ocho = !condicion;
+ocho;
