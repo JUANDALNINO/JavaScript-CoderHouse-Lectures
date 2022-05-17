56d39a810192bbc2769946f2477d48c75dfe8f38
Juan David Lizarralde Niño <nino.davidgo@gmail.com>
2022-05-16 20:38:18 -0500

    Finish the class

--- class04/main.js
+++ class04/main.js
@@ -53,7 +53,61 @@ let res = multiplicacion (numberA, numberB);
 alert(`La multi es ${res}`);
 
 
-// Exercise:
+// Exercise crear calculadora:
+ /*Escriba un programa que permita simular una calculadora. 
+ El usuario debe ingresar una de las siguientes opciones: 
+ 1. Suma 
+ 2. Resta
+ 3. Mul 
+ 4. Div 
+ 5. Fin
+ El menú debe mostrarse, siempre que el usuario no ingrese la opción 5
+ El programa debe mostrar error en los siguientes casos: 
+    A. El usuario no selecciona una opción válida de 1 a 5 
+    B. El usuario no ingresa datos válidos para operar
+    C. El usuario ingresa un '0' como segundo operando para la división
+*/
+
 function mostrarMenu () {
-    
+    let option = 0;
+
+
 }
+
+
+
+// Scope:
+let a = 0;
+
+function miFunction () {
+    let a = 1;
+    console.log(a);
+}
+
+miFunction(); // 1
+console.log(a); // 0
+// Variables locales y globales
+
+
+// Funcion anónima:
+let suma = function (a, b) {
+    return (a + b);
+};
+
+suma (10, 20);
+
+
+// Funciones flecha:
+let resta = (a, b) => {  // La flecha va después de los parámetros y reemplaza la palabra function
+    return (a - b);
+};
+resta (10, 20);
+
+//Si es una función de una sola línea con retorno podemos evitar escribir el cuerpo.
+
+let multiplicacion = (a, b) => a * b;
+// => Ahorrando las llaves y el return.
+
+multiplicacion(3, 4);
+
+
