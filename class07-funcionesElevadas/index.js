bc5727ea8ee2a91839bbac2e6088acc4f79e9119
Juan David Lizarralde Niño <nino.davidgo@gmail.com>
2022-05-30 13:10:24 -0500

    Agree new notes for the lecture

--- class07-funcionesElevadas/index.js
+++ class07-funcionesElevadas/index.js
@@ -26,4 +26,30 @@ function porCadaUno(arr, fun) {
 porCadaUno([1,2,3,4,5,6,7,8,9,10], console.log);
 
 
-// Min => 32
+
+
+// Sistema crud usuarios
+// Create - Read - Update - Delete
+// Admin. de usuarios:
+
+//Objeto usuario
+class Usuario {
+    constructor(id, nombre, apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+//Usuarios
+let usuario1 = new Usuario(1, "Juan", "Niño");
+let usuario2 = new Usuario(2, "Martin", "Angarita");
+let usuario3 = new Usuario(3, "Luisa", "Caceres");
+let usuario4 = new Usuario(4, "Sofia", "Niño");
+let usuario5 = new Usuario(5, "Santiago", "Aguirre");
+
+
+//Array:
+let usuarios = [usuario1, usuario2, usuario3, usuario4, usuario5];
+
+// Min 36
