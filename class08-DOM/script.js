658f4a932457c4a9e08e3fd9650bc92f8ed188c5
Juan David Lizarralde Niño <nino.davidgo@gmail.com>
2022-06-03 15:44:34 -0500

    Init lecture DOM

---
+++ class08-DOM/script.js
@@ -0,0 +1,22 @@
+/*
+DOM
+    => Usar todos los documentos en HTML en JS
+    => Objetos = métodos y propiedades
+
+    => Convertir un documento HTML en un módelo de Objetos para interactuar
+
+h1 => heading más importante
+
+
+
+---> Arbol de nodos
+        Ramas => padres e hijos
+
+*/
+
+//     document. => es un objeto global al cual accedo a un nodo
+// Body = accedo a la etiqueta body
+    const nodoPrincipal = document.body; // => Esto es un objeto
+    console.log(nodoPrincipal);
+
+// min => 20
