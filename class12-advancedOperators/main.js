f1a987688f3624d8263b5db93e403fa353f5c20e
Juan David Lizarralde Niño <nino.davidgo@gmail.com>
2022-06-20 20:00:42 -0500

    Finish the lecture

--- class12-advancedOperators/main.js
+++ class12-advancedOperators/main.js
@@ -63,20 +63,30 @@ usuarios.length !== 0 && alert("Si hay usuarios");
     
     // Exercise:
     // Pedir nombre. Máximo 3 intentos. Imprimir en html.
+    let contador = 1;
     mostrarMensaje();
 
     function mostrarMensaje() {
         document.body.innerHTML="";
 
-        let nombre = prompt("Escriba su nombre");
-        let msj = nombre !=="" ? `Bienvenido, ${nombre}` : `Debe ingresar un nombre`;
+        if(contador<3) {
+            
+            let nombre = prompt("Escriba su nombre");
+            let msj = nombre !=="" ? `Bienvenido, ${nombre}` : `Debe ingresar un nombre`;
+    
+            const div = document.createElement("div");
+            div.innerHTML = msj;
+            document.body.appendChild(div);
+    
+            // Operador And
+            nombre==="" && mostrarBoton();
+    
+            contador++;
 
-        const div = document.createElement("div");
-        div.innerHTML = msj;
-        document.body.appendChild(div);
+        } else {
+            alert("Contacte admin");
+        }
 
-        // Operador And
-        nombre==="" && mostrarBoton();
     }
 
     function mostrarBoton() {
@@ -90,4 +100,106 @@ usuarios.length !== 0 && alert("Si hay usuarios");
     }
 
 
-// 05. 
+
+// 05. Operador Nullish:
+// // operando1 ?? operando2
+// Si es distinto de => null, undefined => Retorna (operador1) 
+// Si no   => Retorna (operador2)
+const usuario3 = {
+    nombre: "John Doe",
+    edad: 14
+}
+const usuario4 = null
+
+console.log(usuario3 ?? "El usuario no existe")
+// { nombre: 'John Doe', edad: 14 }
+console.log( usuario4 ?? "El usuario no existe" )
+// El usuario no existe
+
+
+
+// 06. Acceso condicional
+// ? => No mata el programa hasta ahí
+    let idIngresado = Number(prompt("Ingrese numero"));
+    let buscado = array.find(element => element.id=== idIngresado);
+
+    console.log(buscado?.nombre); // No bota error, bota undefined
+
+
+
+// 07. Destructuración / Destructuring
+// En vez de asignar varias variables a cada propiedad del objeto =>
+    let var1 = prop.objeto;
+    let var2 = prop.objeto2;
+
+    // Lo que se hace es usar:
+    const producto1 = {
+        id: 1,
+        nombre: "leche",
+        precio: 1000,
+    }
+
+    // Variables {var1, var2} = del objeto
+    const {id, nombre, precio} = producto1;
+
+
+
+// 08. Destructuring => Alias
+// Asignarle un alias a las variables
+    const producto2 = {
+        id: 2,
+        nombre: "coke",
+        precio: 3500,
+    }
+
+    // Se le coloca un alias a cada propiedad en cada variable
+    const { 
+        id: idTwo, 
+        nombre: nombreTwo, 
+        precio: precioTwo} = product2;
+
+
+
+// 09. Destructuring Params:
+    const producto3 = {
+        id: 10,
+        nombre: "Curso Javascript",
+        precio: 12500
+    }
+
+    // desestructurando lo que reciba por parámetro
+    const desestructurar = ( {id, nombre} ) => {
+        console.log(id, nombre)
+    }
+    
+    desestructurar(producto3) // 10 Curso Javascript
+
+
+
+// 10. Destructuring array:
+const nombres = ["Juan", "Julieta", "Carlos", "Mariela"]
+
+// equivalente a:
+console.log("Juan", "Julieta", "Carlo", "Mariela")
+
+// spread ... del array
+console.log(...nombres) // Juan Julieta Carlos Mariela
+
+
+
+
+// 11. Spread Operator:
+
+
+
+    // Example 2:
+    const prod1 = {
+        id: 1,
+        nombre: "leche",
+        precio: 2000
+    };
+
+    // No toca ni sobreescribe el prod1
+    const prod2 = {...prod1, id:2};
+    const prod3 = {...prod2, id:3, marca: "clim"};
+
