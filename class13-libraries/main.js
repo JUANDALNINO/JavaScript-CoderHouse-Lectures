9c7507291e9be7a3c76b970bd949e9c68393adfd
Juan David Lizarralde Niño <nino.davidgo@gmail.com>
2022-06-22 15:48:21 -0500

    Init lecture 13

---
+++ class13-libraries/main.js
@@ -0,0 +1,15 @@
+// Libreria:
+/*  - Codigo pre-escrito
+    - Pequeños programas escritos por terceros
+    */
+
+// Implementación:
+    // CDN
+    // Descarga de archivos
+
+// 1. CDN
+// - Importar por script:
+    <script src="link"></script>
+
+// Sweet alert
+    // <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
